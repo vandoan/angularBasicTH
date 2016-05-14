639f361afec10b225fd2ccb5b9ffc0be7532667c
Van Doan <vdoan09@gmail.com>
2016-05-13 23:35:16 -0400

    service section, completed

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,27 @@
 angular.module("todoListApp", [])
 .controller('mainCtrl', function($scope, dataService) {
 
-	$scope.helloConsole = dataService.helloConsole
+	$scope.addToDo = function(){
+		var todo = { name: 'This is a new to do.' };
+		$scope.todos.push(todo);
+	};
 	$scope.angularChange = function() {
 		console.log('A change occured.');
 	};
+
 	dataService.getTodos(function(response){
 		console.log(response.data);
 		$scope.todos = response.data;
 	});
+
+	$scope.deleteTodo = function(todo, $index) {
+		dataService.deleteTodo(todo);
+		$scope.todos.splice($index,1);
+	};
+	$scope.saveTodo = function(todo) {
+		dataService.saveTodo(todo);
+
+	};
 })
 
 .service('dataService', function($http) {
@@ -16,10 +29,16 @@ angular.module("todoListApp", [])
 		console.log('This is the hello console service.');
 
 	};
-	this.getTodos = function(callback){ 
+	this.getTodos = function(callback) { 
 		$http.get('mock/todos.json')
 		.then(callback)
 	}
+	this.deleteTodo = function(todo) {
+		console.log("The " + todo.name + "has been deleted.")
+	};
+	this.saveTodo = function(todo) {
+		console.log("The " + todo.name + "has been saved.")
+	};
 })
 .controller('coolCtrl', function($scope){
 	$scope.whoAmI = function() {
